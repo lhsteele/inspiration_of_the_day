e8e72de92b209d9b22262f3187edcd5213c8cd39
Lisa Steele <lisahsteele@gmail.com>
2020-05-19 21:06:35 -0700

    Adds random generation of NASA image, using the date of the quote.

--- src/components/Inspo.jsx
+++ src/components/Inspo.jsx
@@ -10,31 +10,17 @@ export default class Inspo extends Component {
       image: null,
       imageTitle: '',
       quote: '',
+      quoteDate: '',
       loading: true
     }
   }
 
-  fetchQuote = () => {
-    axios({
-      url: "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand",
-      method: "GET"
-    })
-      .then(response => {
-        const quoteIdx = Math.floor(Math.random() * (9 - 0 + 1)) + 0
-        const randomQuote = response.data[quoteIdx].content.rendered
-        const parsedQuote = parse(randomQuote)[0].props.children
-        this.setState({ quote: parsedQuote, loading: false })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-
-  fetchImage = () => {
+  fetchImage = quoteDate => {
     axios({
       url: "https://api.nasa.gov/planetary/apod",
       method: "GET",
       params: {
+        date: this.state.quoteDate,
         hd: false,
         api_key: "WqcMNCMIfZ1cWl30wZQLKYwpduVm6yHh7Z2u5p5t"
       }
@@ -47,9 +33,26 @@ export default class Inspo extends Component {
       })
   }
 
+
+  fetchQuote = () => {
+    axios({
+      url: "https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand",
+      method: "GET"
+    })
+      .then(response => {
+        const quoteIdx = Math.floor(Math.random() * (9 - 0 + 1)) + 0
+        const randomQuote = response.data[quoteIdx].content.rendered
+        const parsedQuote = parse(randomQuote)[0].props.children
+        const quoteDate = response.data[quoteIdx].date.split("T")[0]
+        this.setState({ quote: parsedQuote, loading: false, quoteDate: quoteDate }, () => this.fetchImage())
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   componentDidMount() {
     this.fetchQuote()
-    this.fetchImage()
   }
 
   displayLoading = () => {
@@ -61,8 +64,10 @@ export default class Inspo extends Component {
 
   render() {
     return (
-      <div className="quote-container">
-        {this.displayLoading()}
+      <div className="container">
+        <div className="quote-container">
+          {this.displayLoading()}
+        </div>
         <img
           src={this.state.image}
           alt={this.state.imageTitle}
